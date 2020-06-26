33a3f4727f847a33ee96c8c681feed0eb189bad9
Nimrod <nimrodl@gmail.com>
2020-06-25 20:27:42 -0400

    moved SECRET_KEY to .env to be loaded automatically using virtualenvwrapper

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uo#y#fl&bfodn41um%5t+pl(3t1l^0=nnj79%)b)c!#ff)%u1#'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
