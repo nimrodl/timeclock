5f9790fcf11e5b414b0d4a3de321c836010115ac
Nimrod <nimrodl@gmail.com>
2020-06-28 15:52:21 -0400

    updated wsgi.py for loading secret key from .env

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/my-project-dir')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
 
 from django.core.wsgi import get_wsgi_application
 
