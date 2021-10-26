a0a5c0f887a62805f5fbd4b954f9541864d6c4d2
nimrod <nimrodl@gmail.com>
2021-10-26 09:30:03 -0400

    limited events shown inline on user page to last 20 days

--- timeclock/admin.py
+++ timeclock/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 
+from datetime import datetime, timedelta
+
 from timeclock.models import User, Event, Schedule
 
 class EventInline(admin.TabularInline):
@@ -9,6 +11,9 @@ class EventInline(admin.TabularInline):
     extra = 1
     readonly_fields = ['get_hours']
     fields = ['date', 'time_in', 'time_out', 'get_hours']
+    def get_queryset(self, request):
+        qs = super(EventInline, self).get_queryset(request)
+        return qs.filter(date__gte=datetime.now()-timedelta(days=20))
 
 class UserAdmin(BaseUserAdmin):
     fieldsets = BaseUserAdmin.fieldsets + (
